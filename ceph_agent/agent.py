e82a731072a91852aedf7113cc9866a10dc4f554
Kritik Sachdeva <sachdevakritik.440@gmail.com>
2025-08-05 21:57:00 +0530

    Restructured the main agent.py to use the class methods

--- ceph_agent/agent.py
+++ ceph_agent/agent.py
@@ -1,15 +1,33 @@
-from core.agent_logic import analyze_response
-from llm.llm_response import run_llm_query_with_ollama
-from rag.semantic_search import search_and_select_command_with_llm
-from utils.file_ops import load_index
+from core.agent_logic import analysePrompt
+from rag.semantic_search import semanticCephSearch
+from utils.file_ops import vectorBuilder
 from ceph.executor import execute_command
+import os
 
 
 def main():
     print("Initializing Ceph Agent...")
     print("--------------------------------")
+    # Set the environment variable to false to disable parallelism
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
     # Step-1: Loading the Database
-    index, metadata, model = load_index("./database/basic_commands.json")
+    vector_store = vectorBuilder(
+        json_path="./database/basic_commands.json",
+        model_name="all-MiniLM-L6-v2",
+        index_path="./faiss_index_store/ceph_faiss.index",
+        metadata_path="./faiss_index_store/ceph_faiss_metadata.json"
+    )
+    # Either retrieve the below parameters as it is as it is already 
+    # retrieved as part of initialisation
+    #index, metadata, model = vector_store._load_index()
+
+    #llm_response = llmResponse()
+    cephSearch = semanticCephSearch(
+        vector_store=vector_store,
+        llm_model="granite3.3:8b",
+        top_k=3,
+        threshold=0.9
+    )
 
     while True:
         # Step-2: Take the user query input
@@ -30,16 +48,10 @@ def main():
         print("--------------------------------")
 
         # Step-4: Search & Select the best available command
-        vector_results, selected_command = search_and_select_command_with_llm(
-            index,
-            metadata,
-            model,
-            user_query,
-            model_choice=model_choice,
-            llm_model="granite3.3:8b",
-            top_k=3,
-            threshold=1.1
-        )  # Here this search_command can become a tool for MCP to use
+        vect_results, selected_command = cephSearch._search_select_with_llm(
+            query=user_query,
+            model_choice=model_choice
+        )
 
         if not selected_command:
             print(
@@ -66,16 +78,17 @@ def main():
             # The selected_purpose_description can be extracted from selected_command_data
             # if you have a specific description of *why* this command was chosen.
             # For simplicity, let's just use the main description text if available.
-            description = next((item['description'] for item in vector_results if item['command'] == selected_command.strip()), 'Description not found.')
-        
-            agent_response = analyze_response(
-                user_query,
-                selected_command,
-                stdout,
-                run_llm_query_with_ollama,
-                description,
-                model_name="granite3.3:8b"
+            description = next((item['description'] for item in vect_results if item['command'] == selected_command.strip()), 'Description not found.')
+            agent = analysePrompt(
+                query=user_query,
+                selected_command=selected_command,
+                command_out=stdout,
+                command_description=description,
+                model_choice=model_choice
             )
+            
+            agent_response = agent._analyze_response()
+
             if not agent_response:
                 agent_response = (
                     "Agent Response: I executed the command, but could not extract "
@@ -85,47 +98,5 @@ def main():
             print("\n------------------------------------")
 
 
-'''
-def main():
-    user_query = "How many service running in the ceph cluster?"
-    print(f"Query: {user_query}")
-    print("--------------------------------")
-
-    index, metadata, model = load_index("./database/basic_commands.json")
-    print("Searching for command...")
-    print("Results:")
-    print("--------------------------------")
-
-    filtered_commands = search_command(index, metadata, model, user_query, top_k=3, model_choice)  # Here this search_command can become a tool for MCP to use
-    if filtered_commands:
-        for r in filtered_commands:
-            print(f"[Score: {r['score']:.4f}] ➜ {r['command']}")
-            print(f"  → {r['description']}\n")
-    else:
-        print("No relevant command found with sufficient confidence.")
-        sys.exit(1)
-
-    # Step 3: Pick which LLM to use
-    model_choice = input("Use Ollama or LM Studio? (o/l): ").strip().lower()
-
-    # Step 4: Use LLM to select best matching command
-    if model_choice == 'o':
-        selected_command = select_command_with_ollama(user_query, filtered_commands, model="llama3")
-    elif model_choice == 'l':
-        selected_command = select_command_with_lmstudio(user_query, filtered_commands)
-    else:
-        print("Invalid choice. Use 'o' for Ollama or 'l' for LM Studio.")
-        return
-
-    # Step 5: Output selected command
-    print(f"\nLLM Selected Command:\n{selected_command.strip()}")
-
-    # Step-6: Execute the selected command on the ceph cluster
-
-    stdout, stderr, retcode = execute_command(selected_command.strip())
-
-'''
-
 if __name__ == "__main__":
     main()
-    print("Done")
